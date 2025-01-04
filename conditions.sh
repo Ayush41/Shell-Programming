ab034047863355b797725e744bd0679f4ade3c40
ayush41 <anonymousking41681@gmail.com>
2025-01-04 21:45:36 +0530

    check 18+ or not

---
+++ conditions.sh
@@ -0,0 +1,14 @@
+# !/bin/bash
+
+echo "Conditions & syntax in bash shell code"
+
+echo "Enter your name :"
+read name
+
+echo "Enter you age :"
+read age
+
+$adult = 18
+
+
+# condition to check if the person is adult or not
