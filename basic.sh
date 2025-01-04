35cf1fe00d31814527ac083a0216c9f773851d52
ayush41 <anonymousking41681@gmail.com>
2025-01-04 21:35:41 +0530

    prime fn

--- basic.sh
+++ basic.sh
@@ -9,3 +9,25 @@ for i in {1..6}
 do
     echo "This is iteration number $i"
 done
+
+# Program to find even number in bash shell programming
+
+# Take user input
+echo "Enter a number :" 
+read num
+
+# function to check prime number
+isPrime(){
+    local n=$1
+    if [$n -eq 1]; then
+    return 1
+    fi
+    for((i=2;i*i<=n;i++)); do
+        if [$((n%i)) -eq 0];
+then 
+    return 1
+    fi
+    done
+    return 0
+}
+
