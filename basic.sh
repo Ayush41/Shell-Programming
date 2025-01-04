322d6b4fbd22b515611b6a562373ebce3d56425a
ayush41 <anonymousking41681@gmail.com>
2025-01-04 20:54:04 +0530

    print echo

--- basic.sh
+++ basic.sh
@@ -1,3 +1,11 @@
 # !/bin/bash
 
+echo "This is a simple bash script"
+
 echo "Welcome to the world of Linux programming"
+
+echo "Looping through a loop in bash shell"
+for i in {1..6}
+do
+    echo "This is iteration number $i"
+done
