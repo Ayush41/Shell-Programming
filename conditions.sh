c5fd2e898edf3aaf3cbd2a0cbcfaf3d4b22cbd2d
ayush41 <anonymousking41681@gmail.com>
2025-01-05 17:27:11 +0530

    checking for user age

--- conditions.sh
+++ conditions.sh
@@ -1,14 +1,26 @@
-# !/bin/bash
+#!/bin/bash
 
 echo "Conditions & syntax in bash shell code"
 
 echo "Enter your name :"
 read name
 
-echo "Enter you age :"
+echo "Enter your age :"
 read age
 
-$adult = 18
-
+adult=18
 
 # condition to check if the person is adult or not
+
+if ! [[ $age =~ ^[0-9]+$ ]] || [ $age -le 0 ]; then
+    echo "Invalid age input"
+    exit 1
+fi
+
+# check if user is an adult
+
+if [ $age -ge $adult ]; then
+    echo "You are an adult"
+else
+    echo "You are not an adult"
+fi
