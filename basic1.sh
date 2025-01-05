388d2857da564f1fb21578e2843c77fa0837e4ca
ayush41 <anonymousking41681@gmail.com>
2025-01-05 17:44:23 +0530

    init

--- basic1.sh
+++ basic1.sh
@@ -2,3 +2,30 @@
 
 echo "This is a bash script basics"
 
+echo "Arithmetic operation :"
+
+#!/bin/bash
+
+# Function to add two numbers
+add() {
+    echo "Addition: $(($1 + $2))"
+}
+
+# Function to subtract two numbers
+subtract() {
+    echo "Subtraction: $(($1 - $2))"
+}
+
+# Function to multiply two numbers
+multiply() {
+    echo "Multiplication: $(($1 * $2))"
+}
+
+# Function to divide two numbers
+divide() {
+    if [ $2 -ne 0 ]; then
+        echo "Division: $(($1 / $2))"
+    else
+        echo "Error: Division by zero is not allowed"
+    fi
+}
