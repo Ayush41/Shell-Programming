c3fc67ff47281c0b10674d4ae0aab2d9fc52f385
ayush41 <anonymousking41681@gmail.com>
2025-01-06 17:46:47 +0530

    file cmds

--- filecmds.sh
+++ filecmds.sh
@@ -5,3 +5,31 @@ echo "All File Commands in Unix/Linux"
 # ls
 echo "ls : List all files in the current directory"
 
+# mkdir dir
+echo "mkdir dir : Create a new directory named dir"
+
+user = ${whoami}
+echo "mkdir -p /home/$user/dir : Create a new directory named dir in the user"
+
+# 10 File commands
+echo "10 File commands"
+echo "1. ls"
+echo "2. mkdir"
+echo "3. rmdir"
+echo "4. touch"
+echo "5. cp"
+echo "6. mv"
+echo "7. rm"
+echo "8. cat"
+echo "9. echo"
+echo "10. less"
+echo "11. more"
+echo "12. head"
+echo "13. tail"
+echo "14. wc"
+echo "15. sort"
+echo "16. uniq"
+echo "17. diff"
+echo "18. grep"
+echo "19. sed"
+echo "20. awk"
