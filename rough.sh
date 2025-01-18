35ef0596bfffaee4defa0829b3f255694de07e3f
ayush41 <anonymousking41681@gmail.com>
2025-01-18 16:54:03 +0530

    loop & conditionals

--- rough.sh
+++ rough.sh
@@ -5,4 +5,17 @@ echo "Syntax of shell scriping"
 name = "RON"
 echo "Hellow, $name"
 
+echo "loooping  trhough number 1 to 5: "
+for i in {1..5}; do
+echo "Number: $i"
+done
+
+echo "Enter a number:"
+read number
+if [$number -gt 10]; then
+echo "The number is greater than 10."
+else
+echo "The number is 10 or less"
+fi
+
 
