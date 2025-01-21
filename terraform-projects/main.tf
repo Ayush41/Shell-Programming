1a04c0ce58442bb053732704c39b936d6743e762
ayush41 <anonymousking41681@gmail.com>
2025-01-22 00:20:59 +0530

    tf template firewall rule subnets

--- terraform-projects/main.tf
+++ terraform-projects/main.tf
@@ -8,9 +8,29 @@ project = "your-gcp-project-id" region = "us-central1"
 resource "google_compute_network" "vpc_network" { name = "terraform-network" }
 
 # Create a Subnetwork resource 
+resource "google_compute_subnetwork" "subnetwork" {
+	name = "terraform-subnetwork"
+	ip_cidr_range = "10.0.0.0/16"
+	region = "us-central1"
+	network = google_compute_network.vpc_network.name	
+	}
 
 # Create a Firewall Rule
+resource "google_compute_firewall" "default" {
+	name = "allow-https"
+	network = google_compute_network.vpc_network.name
+	
+	allow {
+		protocol = "tcp"
+		ports = ["80"]
+			}
+	source_ranges = ["0.0.0.0/0"]	
+	}
 
 # Create a Compute Engine Instance
+
+
+
+
 
 
