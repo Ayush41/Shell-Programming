687bd47b24c0c2740ccfb0420488abdb6a5ae700
ayush41 <anonymousking41681@gmail.com>
2025-02-02 03:11:16 +0530

    input and prints

--- generateRandomNum.sh
+++ generateRandomNum.sh
@@ -2,3 +2,28 @@
 
 echo "Random number generation program"
 
+echo "How many random nubers you wanna generate?"
+read RNUMS
+
+echo "Enter min range:"
+read MIN_RANGE
+
+echo "Enter max range:"
+read MAX_RANGE 
+
+#Generate random number within specific range
+echo "Here are your $NUM_NUMBERS random numbers between $MIN_RANGE and $MAX_RANGE:"
+
+#startfnc(){
+#	echo "Usage: $0 [-n num_numbers] [-r min_range max_range]"
+ #   	echo "  -n num_numbers : The number of random numbers to generate"
+  #  	echo "  -r min_range max_range : The range for the random numbers (inclusive)"
+   # 	exit 1
+#}
+
+
+#generate_randomNum(){
+
+#	echo $((RANDOM % (MAX_RANGE - MIN_RANGE + 1) + MIN_RANGE))		
+
+#}
