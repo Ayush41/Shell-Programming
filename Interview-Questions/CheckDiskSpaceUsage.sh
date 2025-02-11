20dde3007c2b1f9aa1cc77d8d8bfab3b69b741a8
ayush41 <anonymousking41681@gmail.com>
2025-02-12 01:11:08 +0530

    check fnc

--- Interview-Questions/CheckDiskSpaceUsage.sh
+++ Interview-Questions/CheckDiskSpaceUsage.sh
@@ -4,3 +4,9 @@ disk_usage=$(df / | grep / | awk '{ print $5 }' | sed 's/%//g')
 
 # comparing the disk usage and print a warning if it exceeds 80%
 
+if [$disk_usage -gt 80]; then
+	echo "Warning: Disk usage is above 80%. Current usage is $disk_usage%."
+else
+	echo "Disk usage is normal. Current usage is $disk_usage%."
+fi
+
