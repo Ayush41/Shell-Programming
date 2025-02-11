6bb54c4ea5b2bde630055e5657d5ffb866e9e472
ayush41 <anonymousking41681@gmail.com>
2025-02-12 01:15:14 +0530

    init

---
+++ Interview-Questions/Aws-Cli-Auth.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+
+#Write a script to authenticate with aws cli and check if a specific EC2 instance is running.
+#  AWS CLi authentication (assume aws is alreadyy configured)
+
+aws configure
+
+#checking  ec2 instance is running
+
+
