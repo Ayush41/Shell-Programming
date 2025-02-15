50695ffabaaf1b06a2024eefc96f3508156c46a1
ayush41 <anonymousking41681@gmail.com>
2025-02-15 22:36:03 +0530

    gcp storage bucket

--- terraform-projects/gcp-storage-bucket.tf
+++ terraform-projects/gcp-storage-bucket.tf
@@ -1,1 +1,12 @@
+provider "google" {
+  credentials = file("/path/to/your/credentials.json") #changed
+  project     = "adorachat981"
+  region      = "us-central1"
+}
+
+resource "google_storage_bucket" "my_bucket" {
+  name     = "my-unique-bucket-name"
+  location = "US"
+}
+
 
