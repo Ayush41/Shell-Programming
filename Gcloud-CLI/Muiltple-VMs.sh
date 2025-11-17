85240a5e840d850043ca131580281b74b9233896
ayush41 <anonymousking41681@gmail.com>
2025-11-17 19:13:36 +0530

    vm -del,start and stop

--- Gcloud-CLI/Muiltple-VMs.sh
+++ Gcloud-CLI/Muiltple-VMs.sh
@@ -12,3 +12,17 @@ for i in {1..5}; do
     echo "Created VM instance: vm-$1"
 done
 
+
+# To delete the VMs later...
+
+gcloud compute instances delete vm$1 vm-$2 vm-$3 vm-$4 vm-$5 
+    --zone=$zone --quiet
+
+
+# To start All VMs in a PRoject
+
+# Start
+gcloud compute instances start $(gcloud compute instances list --format="value(name)") --zone=us-central1-a
+
+# Stop
+gcloud compute instances stop $(gcloud compute instances list --format="value(name)") --zone=us-central1-a
