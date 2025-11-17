9da540df3e15b7abf90f044e1f6f64e29f97e5ad
ayush41 <anonymousking41681@gmail.com>
2025-11-17 18:51:21 +0530

    gcloud to create multiple instance

--- Gcloud-CLI/Muiltple-VMs.sh
+++ Gcloud-CLI/Muiltple-VMs.sh
@@ -2,3 +2,13 @@
 
 project_id = "project-123456"
 zone = "us-central1-a"
+
+for i in {1..5}; do
+    gcloud compute instances create vm-$1 \
+    --zone=$zone \
+    --machine-type=e2-medium \
+    --image-family=debian-11 \
+    --image-project=debian-cloud
+    echo "Created VM instance: vm-$1"
+done
+
